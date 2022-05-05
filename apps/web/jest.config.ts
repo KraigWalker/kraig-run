9e3e13ac87fec6ae7c7ef20189dbb43d429ced9e
Kraig Walker <kraig_walker@me.com>
2022-05-05 19:27:24 +0100

    run necessary nx v14 migrations

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,10 +1,11 @@
 module.exports = {
   displayName: 'web',
-  preset: '../../jest.preset.js',
+
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/web',
+  preset: '../../jest.preset.ts',
 };
