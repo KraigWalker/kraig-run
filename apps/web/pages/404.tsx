7d9527c89b3520a814499e4ad7976e7c1188b458
Kraig Walker <kraig_walker@me.com>
2022-05-06 16:19:28 +0100

    add 404 and fix dots svg path

---
+++ apps/web/pages/404.tsx
@@ -0,0 +1,12 @@
+import styles from './index.module.css';
+
+export function NotFound() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return <div className={styles.page}>404 Not Found</div>;
+}
+
+export default NotFound;
