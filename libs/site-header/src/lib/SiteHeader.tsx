a8f902e2a871fc415553b770d9c38935ea0a15c6
Kraig Walker <kraig_walker@me.com>
2022-05-17 18:06:29 +0100

    add site footer

--- libs/site-header/src/lib/SiteHeader.tsx
+++ libs/site-header/src/lib/SiteHeader.tsx
@@ -1,4 +1,5 @@
 import styles from './SiteHeader.module.css';
+import Link from 'next/link';
 
 /* eslint-disable-next-line */
 export interface SiteHeaderProps {}
@@ -6,13 +7,27 @@ export interface SiteHeaderProps {}
 export function SiteHeader(props: SiteHeaderProps) {
   return (
     <header role="banner" className={styles['banner']}>
+      <div className={styles['border']}>
+        <div className={styles['top-border']}></div>
+        <div className={styles['bottom-border']}></div>
+        <div className={styles['left-border']}></div>
+        <div className={styles['top-border']}></div>
+      </div>
       <h1 className={styles['title']}>kraig.run</h1>
       <nav>
         <ul>
-          <li>Journal</li>
-          <li>Stats</li>
-          <li>About</li>
-          <li>Gear</li>
+          <li>
+            <Link href="/log">Log</Link>
+          </li>
+          <li>
+            <Link href="/stats">Stats</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/gear">Gear</Link>
+          </li>
         </ul>
       </nav>
     </header>
