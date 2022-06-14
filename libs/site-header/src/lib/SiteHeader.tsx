4dd20401f640e38339bd291b4fc0008bb20e7e26
Kraig Walker <kraig_walker@me.com>
2022-06-14 19:24:52 +0100

    clear header and background

--- libs/site-header/src/lib/SiteHeader.tsx
+++ libs/site-header/src/lib/SiteHeader.tsx
@@ -1,4 +1,4 @@
-import styles from './SiteHeader.module.css';
+import { Header, Heading }  from '@adobe/react-spectrum';
 import Link from 'next/link';
 
 /* eslint-disable-next-line */
@@ -6,31 +6,9 @@ export interface SiteHeaderProps {}
 
 export function SiteHeader(props: SiteHeaderProps) {
   return (
-    <header role="banner" className={styles['banner']}>
-      <div className={styles['border']}>
-        <div className={styles['top-border']}></div>
-        <div className={styles['bottom-border']}></div>
-        <div className={styles['left-border']}></div>
-        <div className={styles['top-border']}></div>
-      </div>
-      <h1 className={styles['title']}>kraig.run</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/log">Log</Link>
-          </li>
-          <li>
-            <Link href="/stats">Stats</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/gear">Gear</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Header>
+      <Heading level={1}>kraig.run</Heading>
+    </Header>
   );
 }
 
